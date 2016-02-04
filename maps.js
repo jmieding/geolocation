89dd0739162c17bfe10e16e291e7e5f8a7154701
JMieding <jtmieding@gmail.com>
2016-02-04 14:37:05 -0600

    Adjust html layout, add div displaying lat, long, error info, add error handling to getCurrentPosition call in maps.js

--- maps.js
+++ maps.js
@@ -10,14 +10,33 @@ function newMap(map, latitude, longitude) {
   });
 }
 
+function newInfo(latitude, longitude, locAccuracy) {
+  var output = document.getElementById("info");
+  var locAccuracy = Math.round(locAccuracy *= 3.28084);
+  output.innerHTML = "<p>Latitude:<BR>" + latitude + "<BR><BR>Longitude:<BR>" + longitude + 
+  "<BR><BR>Margin of error:<BR>" + locAccuracy + " feet(+/-)</p>";
+}
+
+function success(position) {
+  var latitude = position.coords.latitude;
+  var longitude = position.coords.longitude;
+  var locAccuracy = position.coords.accuracy;
+  var map;
+  newMap(map, latitude, longitude);
+  newInfo(latitude, longitude, locAccuracy);
+}
+
+function failure(err) {
+  if (err.code == 1) {
+    alert("Your browser is not allowing this site to locate your computer. If you want to change this, refresh the page, click the 'locate' button again, and click 'allow' when your browser asks for your permission.");
+  } else {
+    alert("Your location is unavailable.")
+  }
+}
+
 function locate() {
   if ("geolocation" in navigator) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var latitude = position.coords.latitude;
-      var longitude = position.coords.longitude;
-      var map;
-      newMap(map, latitude, longitude);
-    });       
+    navigator.geolocation.getCurrentPosition(success, failure, {timeout:70000}); 
   } else {
     alert("Sorry, your browser doesn't support geolocation.");
     return;
